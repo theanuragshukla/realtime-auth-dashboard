b379f15b55bb396254879edc24a1ccabdd2eaa0c
theanuragshukla <tachyonanurag@gmail.com>
2024-05-06 16:16:55 +0530

    🔥 fixed blocking

--- server/src/schemas/authSchemas.ts
+++ server/src/schemas/authSchemas.ts
@@ -3,21 +3,7 @@ import { z } from "zod";
 export const loginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(1).max(128),
-  device_details: z.object({
-    ipAddress: z.string().ip({ version: 'v6' }),
-    countryName: z.string(),
-    countryCode: z.string(),
-    region: z.string(),
-    city: z.string(),
-    cityLatLong: z.string(),
-    browser: z.string(),
-    browserVersion: z.string(),
-    deviceBrand: z.string(),
-    deviceModel: z.string(),
-    deviceFamily: z.string(),
-    os: z.string(),
-    osVersion: z.string(),
-  }),
+  device_details: z.any(),
 });
 
 export const registerSchema = z.object({
@@ -25,19 +11,5 @@ export const registerSchema = z.object({
   password: z.string().min(6).max(128),
   name: z.string(). min(3).max(128),
   twofactor: z.boolean(),
-  device_details: z.object({
-    ipAddress: z.string().ip({ version: 'v6' }),
-    countryName: z.string(),
-    countryCode: z.string(),
-    region: z.string(),
-    city: z.string(),
-    cityLatLong: z.string(),
-    browser: z.string(),
-    browserVersion: z.string(),
-    deviceBrand: z.string(),
-    deviceModel: z.string(),
-    deviceFamily: z.string(),
-    os: z.string(),
-    osVersion: z.string(),
-  }),
+  device_details: z.any(),
 });
